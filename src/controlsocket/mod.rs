ab03856cb3ca3fffb279200627eaa1ef08c86cee
dust <du5t@multiplexed.be>
2015-10-09 15:55:52 +0200

    initial domainsocket implementation of controlport trait

--- src/controlsocket/mod.rs
+++ src/controlsocket/mod.rs
@@ -1,5 +1,6 @@
 extern crate bufstream;
 extern crate chrono;
+extern crate unix_socket;
 use std::net::*;
 use std::error::Error;
 use std::io::{Write, BufRead}; // Read also?
@@ -7,6 +8,7 @@ use std::io;
 // use std::net::{TcpStream, SocketAddr, IpAddr, Ipv4Addr, Ipv6Addr, Shutdown};
 use self::bufstream::{BufStream}; // IntoInnerError also once we handle it well
 use self::chrono::*;
+use self::unix_socket::UnixStream;
 
 // TODO store socket buffer size (1024?) as a constant out here somewhere
 // socket buffer size should be at least 2048 re: tor dev list discussion
@@ -26,17 +28,77 @@ trait ControlSocket<'a> {
     fn close(self) -> Result<(), io::Error>;
 }
 
+struct DomainSocket {
+    socket_path: String,
+    buf_stream: BufStream<UnixStream>,
+    buffer: Vec<u8>,
+    time_connected: DateTime<UTC>
+}
+
 struct ControlPort {
     address: SocketAddr,
     buf_stream: BufStream<TcpStream>,
     buffer: Vec<u8>,
     time_connected: DateTime<UTC>
-//    connected: bool
+}
+
+impl DomainSocket {
+    fn new(dest_path: &str) -> io::Result<DomainSocket> {
+        let socket = try!(UnixStream::connect(dest_path));
+        let buf_stream = BufStream::new(socket);
+        let vec_buffer = Vec::<u8>::with_capacity(2048);
+        Ok(DomainSocket {
+            socket_path: dest_path.to_string(),
+            buf_stream: buf_stream,
+            buffer: vec_buffer,
+            time_connected: UTC::now()
+        })
+    }
+}
+
+impl<'a> ControlSocket<'a> for DomainSocket {
+    fn get_address(&mut self) -> String {
+        self.socket_path.to_string()
+    }
+    fn get_port(&mut self) -> u16 {
+        // TODO express better that sockets have no port or otherwise refactor
+        return 0;
+    }
+    fn send(&'a mut self, message: &str) -> Result<(), io::Error> {
+        self.buf_stream.write_all(message.as_bytes())
+    }
+    fn recv(&'a mut self) -> Result<usize, io::Error> {
+        self.buf_stream.read_until(b'\r', &mut self.buffer)
+    }
+    // fn is_alive(&'a self) -> bool;
+    fn is_localhost(&mut self) -> bool {
+        // TODO if there is some crazy remote socket stuff going on, check for
+        // it
+        true
+    }
+    fn connection_time(&'a mut self) -> Duration {
+        chrono::UTC::now() - self.time_connected
+    }
+    // fn connect(&self); // TODO need this kind of stateful info? new == connect?
+    fn close(self) -> Result<(), io::Error>{
+        // TODO unwrap call here: need to handle two possible error types:
+        // IntoInnerError<BufStream<TcpStream>> and io::Error
+         // let tcp_stream = match self.buf_stream.into_inner() {
+         //     Ok(stream) => stream,
+         //     Err(err) => return err
+         // };
+         // match tcp_stream.shutdown(Shutdown::Both) {
+         //     Ok(sb) => sb,
+         //     Err(err) => return err
+        // }
+        self.buf_stream.into_inner()
+            .unwrap()
+            .shutdown(Shutdown::Both)
+    }
 }
 
 impl ControlPort {
     fn new(dest_addr: &str, dest_port: u16) -> io::Result<ControlPort> {
-        // TODO handle this error, don't unwrap
         let stream = try!(TcpStream::connect((dest_addr, dest_port)));
         // this should be ok if above ok
         let address = stream.peer_addr().unwrap();
@@ -77,7 +139,7 @@ impl<'a> ControlSocket<'a> for ControlPort {
             IpAddr::V6(address) => address.is_loopback()
         }
     }
-    fn connection_time(&mut self) -> chrono::duration::Duration {
+    fn connection_time(&mut self) -> Duration {
         chrono::UTC::now() - self.time_connected
     }
     fn close(self) -> Result<(), io::Error>{
